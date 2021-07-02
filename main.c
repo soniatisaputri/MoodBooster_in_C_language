920146493c563d07a9b53d328526660892cc90e7
Sonia Tisa Putri <49554864+soniatisaputri@users.noreply.github.com>
2021-07-02 13:40:54 +0700

    first

---
+++ main.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	
+	sleep(2);
+	printf("\t\tHALO SONIA ^_^\n");
+	sleep(3);
+	printf("\t\tApa kabar?\n");
+	sleep(3);
+	printf("\t\tLagi Sedih?\n");
+	sleep(3);
+	printf("\t\tTenang... Aku ada hiburan buat kamu ^_^\n\n");
+	sleep(1);
+	int A[3]={1,2,3};
+	int b;
+	for (b=0;b<3;b++){
+		sleep(1);		
+		printf("\t\t\t%d\n",A[b]);
+		sleep(1);
+	}	
+//	system("pause");
+    system("cls");
+    system("COLOR A0");
+	int a;
+    for (a=0;a<5;a++){	
+			sleep(1);   
+			printf("\t\t======  =====  ======    ====     ||:::::\n");
+			printf("\t\t  ||    |        ||    : ==== :   ||   ::\n");
+			printf("\t\t  ||    |===     ||   :        :  ||:::::\n");
+			printf("\t\t  ||    |====    ||  :          : ||     \n");	
+			printf("\t\t================SEMANGAT ^_^==================");
+			printf("\n\n");
+			printf("\t\t=========      ====     : :    :  ======   ====     ||:::::     \n");
+			printf("\t\t== : : ==    : ==== :   :  :   :    ||    :==== :   ||   ::       \n");
+			printf("\t\t==:   :==   :        :  :   :  :    ||   :       :  ||:::::       \n");
+			printf("\t\t=:     :== :          : :    : :    ||  :         : ||         \n");
+			printf("\n\n");
+			printf("\t\t=========      ====     : :    :  ======   ====     ||:::::     \n");
+			printf("\t\t== : : ==    : ==== :   :  :   :    ||    :==== :   ||   ::       \n");
+			printf("\t\t==:   :==   :        :  :   :  :    ||   :       :  ||:::::       \n");
+			printf("\t\t=:     :== :          : :    : :    ||  :         : ||         \n");
+			printf("\t\t================Mantap Djiwa==================");
+			sleep(1); 
+			system("cls");
+		}
+		
+	char B[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+	int c,k;
+	for ( c=0;c<3;c++){
+		for (k = 0;k<4;k++){
+			if(c==0 && k==0){
+			system("COLOR B0");
+			 printf("%d = Jomblo fisabilillah itu baik :) \n",B[c][k]);	
+			 sleep(1);
+			}
+			if(c==0 && k==1){
+			system("COLOR A0");
+			 printf("%d = Jadilah pribadi yang baik ;) \n",B[c][k]);
+			 sleep(1);	
+			}
+			 
+			if (c==0 && k==2){
+				system("COLOR B0");
+			 printf("%d = Hormati orang tua anda :D \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			if(c==0 && k==3){
+				system("COLOR C0");
+			 printf("%d = Hormati orang lain ;) \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			if(c==0 && k==4){
+				system("COLOR D0");
+			 printf("%d = Tersenyum saat menghadapi masalah ;) \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			if(c==1 && k==0){
+				system("COLOR E0");
+			 printf("%d = Bersyukur atas apa yang didapat \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			if(c==1 && k==1){
+				system("COLOR F0");
+			 printf("%d = Berteman dengan baik ^_^ \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			if(c==1 && k==2){
+				system("COLOR A0");
+			 printf("%d = Mengejar ridho Allah \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			if(c==1 && k==3){
+				system("COLOR B0");
+			 printf("%d = Sabar adalah kunci utama sukses ;) \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			if(c==1 && k==4){
+				system("COLOR C0");
+			 printf("%d = Tekun :) \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			if(c==2 && k==0){
+				system("COLOR D0");
+			 printf("%d = Jangan lupa istirahat kalau lelah \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			if(c==2 && k==1){
+				system("COLOR E0");
+			 printf("%d = Don't STOP when you're tired, but stop when you are DONE \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			if(c==2 && k==2){
+				system("COLOR F0");
+			 printf("%d = Menghargai orang lain :) \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			 if(c==2 && k==3){
+			 	system("COLOR A0");
+			 printf("%d = Disiplin :) \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			 if(c==2 && k==4){
+			 	system("COLOR B0");
+			 printf("%d = Waktu adalah PEDANG :) \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			 if(c==3 && k==0){
+			 	system("COLOR C0");
+			 printf("%d = Main game secukupnya ya \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			 if(c==3 && k==1){
+			 	system("COLOR D0");
+			 printf("%d = Main game di dunia nyata lebih asyik dari pada di dunia maya \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			 if(c==3 && k==2){
+			 	system("COLOR E0");
+			 printf("%d = Tuhan akan selalu menjagamu \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+			 if(c==3 && k==3){
+			 	system("COLOR F0");
+			 printf("%d = Tetaplah kuat \n",B[c][k]);
+			 sleep(1);	
+			}
+			
+			 if(c==3 && k==4){
+			 	system("COLOR A0");
+			 printf("%d = Karena kamu telah diciptakan kuat :) \n",B[c][k]);	
+			 sleep(1);
+			}
+			
+	
+}
+}
+	return 0;
+}
